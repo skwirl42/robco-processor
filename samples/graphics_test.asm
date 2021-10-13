eb92ee0d2402403b36b9b796f80c3d46aaed1442
James Dessart <skwirl42@gmail.com>
2021-10-13 12:16:18 -0300

    Updated graphics_test.asm from merge

--- samples/graphics_test.asm
+++ samples/graphics_test.asm
@@ -6,26 +6,26 @@
 start:
 	pushiw GRAPHICS_START
 	pullx
-	;pushi 0x97				; 480x320, 8 bpp, border on - 153,600 bytes, should always fail
-	;pushi 0x95				; 480x320, 4 bpp, border on - 76,800 bytes, should always fail
-	;pushi 0x93				; 480x320, 2 bpp, border on - 38,400 bytes, should fit if GRAPHICS_START is low enough
-	;pushi 0x91				; 480x320, 1 bpp, border on - 19,200 bytes, should fit easily
-	;pushi 0x77				; 320x240, 8 bpp, border on - 76,800 bytes, should always fail
-	;pushi 0x65				; 320x240, 4 bpp, border off - 38,400 bytes
-	;pushi 0x73				; 320x240, 2 bpp, border on - 19,200 bytes
-	;pushi 0x71				; 320x240, 1 bpp, border on - 9,600 bytes
-	pushi 0x47				; 240x160, 8 bpp, border off - 38,400 bytes
-	;pushi 0x45				; 240x160, 4 bpp, border off - 19,200 bytes
-	;pushi 0x43				; 240x160, 2 bpp, border off - 9,600 bytes
-	;pushi 0x41				; 240x160, 1 bpp, border off - 4,800 bytes
-	;pushi 0x37				; 192x128, 8 bpp, border on - 24,576 bytes
-	;pushi 0x35				; 192x128, 4 bpp, border on - 12,288 bytes
-	;pushi 0x33				; 192x128, 2 bpp, border on - 6,144 bytes
-	;pushi 0x31				; 192x128, 1 bpp, border on - 3,072 bytes
-	;pushi 0x17				; 120x80, 8 bpp, border on - 9,600 bytes
-	;pushi 0x15				; 120x80, 4 bpp, border on - 4,800 bytes
-	;pushi 0x13				; 120x80, 2 bpp, border on - 2,400 bytes
-	;pushi 0x11				; 120x80, 1 bpp, border on - 1,200 bytes
+	;pushi 0x3C				; 480x320, 8 bpp, border on - 153,600 bytes, should always fail
+	;pushi 0x2C				; 480x320, 4 bpp, border on - 76,800 bytes, should always fail
+	;pushi 0x1C				; 480x320, 2 bpp, border on - 38,400 bytes, should fit if GRAPHICS_START is low enough
+	;pushi 0x0C				; 480x320, 1 bpp, border on - 19,200 bytes, should fit easily
+	;pushi 0x3B				; 320x240, 8 bpp, border on - 76,800 bytes, should always fail
+	pushi 0x23				; 320x240, 4 bpp, border off - 38,400 bytes
+	;pushi 0x1B				; 320x240, 2 bpp, border on - 19,200 bytes
+	;pushi 0x0B				; 320x240, 1 bpp, border on - 9,600 bytes
+	;pushi 0x32				; 240x160, 8 bpp, border off - 38,400 bytes
+	;pushi 0x22				; 240x160, 4 bpp, border off - 19,200 bytes
+	;pushi 0x12				; 240x160, 2 bpp, border off - 9,600 bytes
+	;pushi 0x02				; 240x160, 1 bpp, border off - 4,800 bytes
+	;pushi 0x39				; 192x128, 8 bpp, border on - 24,576 bytes
+	;pushi 0x29				; 192x128, 4 bpp, border on - 12,288 bytes
+	;pushi 0x19				; 192x128, 2 bpp, border on - 6,144 bytes
+	;pushi 0x09				; 192x128, 1 bpp, border on - 3,072 bytes
+	;pushi 0x38				; 120x80, 8 bpp, border on - 9,600 bytes
+	;pushi 0x28				; 120x80, 4 bpp, border on - 4,800 bytes
+	;pushi 0x18				; 120x80, 2 bpp, border on - 2,400 bytes
+	;pushi 0x08				; 120x80, 1 bpp, border on - 1,200 bytes
 	syscall GRAPHICSTART
 	pushi 0
 	cmp
